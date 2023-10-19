6270033cf99574a226f111856c00f2aca8527b5a
Sean McQuaid <sean.mcquaid@cfacorp.com>
2023-10-19 15:53:00 -0400

    fix up slides and rtk code

--- src/rtk-query/src/store/index.ts
+++ src/rtk-query/src/store/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
+import postsApi from "./postsApi";
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(postsApi.middleware),
 });
 
 export default store;
--- src/rtk-query/src/store/postsApi/index.ts
+++ src/rtk-query/src/store/postsApi/index.ts
@@ -1,21 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import Post from "../../types/Post";
 
-// ADD KEYS LATER
 const postsApi = createApi({
   reducerPath: "postsApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://jsonplaceholder.typicode.com/",
   }),
+  tagTypes: ['Posts'],
   endpoints: (builder) => ({
     getPosts: builder.query<Post[], void>({
       query: () => "posts",
+      providesTags: result =>
+        result
+          ? result.map(({ id }) => ({ type: 'Posts', id }))
+          : [{ type: 'Posts', id: 'LIST' }],
     }),
     deletePost: builder.mutation<void, number>({
       query: (id) => ({
         url: `posts/${id}`,
         method: "DELETE",
       }),
+      invalidatesTags: ['Posts'],
     }),
   }),
 });
