847c2977d677db7f53612419b313facc2d4c7478
seanmcquaid <sean.m.mcquaid@gmail.com>
2023-10-23 21:32:04 -0400

    finish

--- src/react-query/src/services/postsService.ts
+++ src/react-query/src/services/postsService.ts
@@ -7,6 +7,6 @@ const postsService = {
     fetch(`${baseUrl}/posts`).then((res) => res.json() as Promise<Post[]>),
   deletePost: (id: number) =>
     fetch(`${baseUrl}/posts/${id}`, { method: "DELETE" }),
-};
+} as const;
 
 export default postsService;
